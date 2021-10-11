35cf1712453067501834a966f71df39989cbb9a3
chrisdavid32 <chrisdavid3271@gmail.com>
2021-10-11 17:01:42 +0100

    fixed edit Admin profile

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,6 +1,7 @@
 
 @extends('admin.admin_master')
 @section('admin')
+<script src="{{ asset('js/jquery.js')}}"></script>
 <div class="container-full">
 
 		<section class="content">
@@ -12,15 +13,14 @@
 						</div>
 			<!-- /.box-header -->
 						<div class="box-body">
-								<div class="row">
-										<div class="col">
+
 												<form novalidate="">
 																<div class="row">
 																					<div class="col-md-6" >
 																					<div class="form-group">
 																					<h5>Admin UserName <span class="text-danger">*</span></h5>
 																					<div class="controls">
-																					<input type="text" name="name" class="form-control" required="">
+																					<input type="text" name="name" value="{{$editData->name}}" class="form-control" required="">
 																						</div>
 																				</div>
 																				</div>
@@ -28,37 +28,61 @@
 																<div class="form-group">
 																<h5>Admin email <span class="text-danger">*</span></h5>
 																<div class="controls">
-																<input type="email" name="email" class="form-control" required="">
+																<input type="email" name="email" value="{{$editData->email}}" class="form-control" required="">
 															</div>
 															</div>
 															</div>
+																</div>
+															<div class="row">
+																	<div class="col-md-6" >
 															<div class="form-group">
 															<h5>File Input Field <span class="text-danger">*</span></h5>
 															<div class="controls">
-																<input type="file" name="file" class="form-control" required=""> <div class="help-block">
-
+																<input type="file" name="file" class="form-control" id="image" required=""> 
+															</div>
+															</div>
+																	</div>
+																<div class="col-md-6" >	
+																	<img id="showImage"	src="{{ (!empty($editData->profile_photo_path)) ? url('upload/admin_image/'.$editData->profile_photo_path) : url('upload/default.jpg')}}" style="width: 100px; height: 100px">
 																</div>
-														</div>
-
-														<div class="row mt-3">
-															<div class="col-md-6" >
-														<div class="text-xs-right">
+															</div>
+												
+															<div class="row">
+																<div class="col-md-10">
+																	<div class="mt-3 row">
+															<div class="text-xs-right">
 														<input type="submit"  class="mb-5 btn btn-rounded btn-primary" value="Update">
 													</div>
 													</div>
-													</div>
+																</div>
+
+														
+												
 												</form>
 
-										</div>
-							<!-- /.col -->
-								</div>
+									
 			  <!-- /.row -->
 			</div>
 			<!-- /.box-body -->
 		  </div>
 		  <!-- /.box -->
-
+				</div>
+		</div>
 		</section>
-	  </div>
+	  
+	</div>
+			<script type="text/javascript">
+$(document).ready(function(){
+$('#image').change(function(e){
+let reader = new FileReader();
+reader.onload = function(e){
+	$('#showImage').attr('src', e.target.result);
+}
+reader.readAsDataURL(e.target.files['0']);
+});
+
+});
+  
+</script>
 
    @endsection

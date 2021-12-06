b033cb11a8c2013c7e3947d04da18dd6a8eb7893
chrisdavid32 <chrisdavid3271@gmail.com>
2021-12-06 22:48:49 +0100

    add brands module

--- app/Http/Controllers/Backend/brandController.php
+++ app/Http/Controllers/Backend/brandController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use Illuminate\Http\Request;
 
 class brandController extends Controller
 {
-    //
+
+    public function brandView()
+    {
+        $brands = Brand::latest()->get();
+        return view('backend.brand_view', compact('brands'));
+    }
 }

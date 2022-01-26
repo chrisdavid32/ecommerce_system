49414a773e552546d469849405222815672edd08
chrisdavid32 <chrisdavid3271@gmail.com>
2022-01-27 00:55:12 +0100

    added category

--- app/Http/Controllers/Backend/categoryContoller.php
+++ app/Http/Controllers/Backend/categoryContoller.php
@@ -36,4 +36,40 @@ class categoryContoller extends Controller
         ];
         return redirect()->back()->with($notification);
     }
+
+    public function categoryEdit($id)
+    {
+        $category = Category::findOrFail($id);
+        return view('backend.category_edit', compact('category'));
+    }
+
+    public function categoryUpdate(Request $request)
+    {
+        $cart_id = $request->id;
+        $category = Category::findOrFail($cart_id);
+        $category->update([
+            'category_name_en' => $request->category_en,
+            'category_name_hin' => $request->category_hin,
+            'category_slug_en' => strtolower(str_replace(' ', '_', $request->category_en)),
+            'category_slug_hin' => str_replace(' ', '_', $request->category_hin),
+            'caterogy_icon' => $request->caterogy_icon
+        ]);
+
+        $notification = [
+            'message' => 'Category updated Successfully',
+            'alert-type' => 'success'
+        ];
+        return redirect()->route('all.category')->with($notification);
+    }
+
+    public function categoryDelete($id)
+    {
+        Category::findOrFail($id)->delete();
+        $notification = [
+            'message' => 'Category Deleted Successfully',
+            'alert-type' => 'success'
+        ];
+
+        return redirect()->back()->with($notification);
+    }
 }

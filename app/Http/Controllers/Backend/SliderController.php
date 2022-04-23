b4e8970411abc1fdcde3ced29d88b9350816dd20
chrisdavid32 <chrisdavid3271@gmail.com>
2022-04-23 22:33:09 +0100

    slider module

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Slider;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Image;
 
 class SliderController extends Controller
 {
     public function sliderView()
     {
-        
+        $slider = Slider::latest()->get();
+        return view('backend.slider.slider_view', compact('slider'));
     }
 }

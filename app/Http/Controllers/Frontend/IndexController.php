e90ebf2b061467ac632d5b88d57b41ae5c2b7a90
chrisdavid32 <chrisdavid3271@gmail.com>
2022-05-04 17:03:23 +0100

    product skip detail

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -6,11 +6,13 @@ use App\Models\User;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use App\Models\multiimg;
 use App\Models\Product;
 use App\Models\Slider;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use phpDocumentor\Reflection\Types\Null_;
 
 class IndexController extends Controller
 {
@@ -21,7 +23,7 @@ class IndexController extends Controller
         $sliders = Slider::where('status', 1)->orderBy('id', 'DESC')->limit(3)->get();
         $categories = Category::orderBy('category_name_en', 'ASC')->get();
         $feature = Product::where('featured', 1)->orderBy('id', 'DESC')->limit(6)->get();
-        $hot_deals = Product::where('hot_deals', 1)->orderBy('id', 'DESC')->limit(3)->get();
+        $hot_deals = Product::where('hot_deals', 1)->where('discount_price', '!=', Null)->orderBy('id', 'DESC')->limit(3)->get();
         $special = Product::where('special_offer', 1)->orderBy('id', 'DESC')->limit(3)->get();
         $special_deals = Product::where('special_deals', 1)->orderBy('id', 'DESC')->limit(3)->get();
         $skip_category_first = Category::skip(0)->first();
@@ -29,8 +31,12 @@ class IndexController extends Controller
         $skip_category_second = Category::skip(1)->first();
         $skip_product_second = Product::where('status', 1)->where('category_id', $skip_category_second->id)->orderBy('id', 'DESC')->get();
 
+        //brand skip
+        $skip_brand_second = Brand::skip(1)->first();
+        $skip_product_second = Product::where('status', 1)->where('brand_id', $skip_brand_second->id)->orderBy('id', 'DESC')->get();
+
         return view('frontend.index', compact('categories', 'sliders', 'products', 'feature', 'hot_deals',
-         'special', 'special_deals', 'skip_category_first', 'skip_product_first', 'skip_category_second', 'skip_product_second'));
+         'special', 'special_deals', 'skip_category_first', 'skip_product_first', 'skip_category_second', 'skip_product_second', 'skip_brand_second', 'skip_product_second'));
     }
 
     public function userLogout()
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -840,6 +840,80 @@
     </div>
     <!-- /.wide-banners -->
     <!-- ============================================== WIDE PRODUCTS : END ============================================== -->
+
+    <!-- Brand skin product-->
+ <!-- Skip product first--->
+ <section class="section featured-product wow fadeInUp">
+  <h3 class="section-title">{{$skip_brand_second->brand_name_en}}</h3>
+  <div class="owl-carousel home-owl-carousel custom-carousel owl-theme outer-top-xs">
+    @foreach ($skip_product_second as $product)
+  
+    <div class="item item-carousel">
+     <div class="products">
+      <div class="product">
+       <div class="product-image">
+        <div class="image"> <a href="{{ url('product/details/'.$product->id) }}"><img src="{{ asset($product->product_thumbnail)}}" alt=""></a> </div>
+        <!-- /.image -->
+        @php
+         $discount = ($product->discount_price/$product->selling_price) * 100;
+        @endphp
+
+          @if($product->discount_price == NULL)
+          <div class="tag new"><span>new</span></div>
+      
+        @else
+        <div class="tag hot"><span>{{ round($discount) }}%</span></div>
+        @endif
+       </div>
+       <!-- /.product-image -->
+
+       <div class="text-left product-info">
+        <h3 class="name"><a href="{{ url('product/details/'.$product->id) }}">{{$product->product_name_en}}</a></h3>
+        <div class="rating rateit-small"></div>
+        <div class="description"></div>
+        @if($product->discount_price == NULL)
+        <div class="product-price"> <span class="price">&#8358; {{$product->selling_price}} </span> </div>
+        @else
+        <div class="product-price"> <span class="price"> &#8358; {{$product->selling_price - $product->discount_price}}</span> <span class="price-before-discount">
+          &#8358;{{$product->selling_price}}</span> </div>
+        @endif
+
+        
+        <!-- /.product-price -->
+
+       </div>
+       <!-- /.product-info -->
+       <div class="clearfix cart animate-effect">
+        <div class="action">
+         <ul class="list-unstyled">
+          <li class="add-cart-button btn-group">
+           <button data-toggle="tooltip" class="btn btn-primary icon" type="button" title="Add Cart"> <i
+             class="fa fa-shopping-cart"></i> </button>
+           <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
+          </li>
+          <li class="lnk wishlist"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html"
+            title="Wishlist"> <i class="icon fa fa-heart"></i> </a> </li>
+          <li class="lnk"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Compare"> <i
+             class="fa fa-signal" aria-hidden="true"></i> </a> </li>
+         </ul>
+        </div>
+        <!-- /.action -->
+       </div>
+       <!-- /.cart -->
+      </div>
+      <!-- /.product -->
+
+     </div>
+     <!-- /.products -->
+    </div>
+   @endforeach
+   <!-- /.item -->
+
+  </div>
+  <!-- /.home-owl-carousel -->
+ </section>
+<!-- End Skip product-->
+    <!-- End Brand Skin product-->
     <!-- ============================================== BEST SELLER ============================================== -->
 
     <div class="best-deal wow fadeInUp outer-bottom-xs">

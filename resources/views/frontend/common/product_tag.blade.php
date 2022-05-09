db28a6f6185285097d9d7edd20bd9be280bc8707
chrisdavid32 <chrisdavid3271@gmail.com>
2022-05-09 21:19:11 +0100

    product tag

--- resources/views/frontend/common/product_tag.blade.php
+++ resources/views/frontend/common/product_tag.blade.php
@@ -1,20 +1,13 @@
 @php
     $tags_name = App\Models\Product::groupBy('product_tags_en')->select('product_tags_en')->get();
-@endphp
-
-<!-- ============================================== PRODUCT TAGS ============================================== -->
-<div class="sidebar-widget product-tag wow fadeInUp">
-    <h3 class="section-title">Product tags</h3>
-    <div class="sidebar-widget-body outer-top-xs">
-
-        @foreach ($tags_name as $tag)
-                 
-     <div class="tag-list"> <a class="item" title="Phone" href="category.html">{{$tag->product_tags_en}}</a>
-        @endforeach
-           <!-- /.tag-list -->
+    @endphp
+    <div class="sidebar-widget product-tag wow fadeInUp">
+     <h3 class="section-title">Product tags</h3>
+     <div class="sidebar-widget-body outer-top-xs">
+      @foreach ($tags_name as $tag)
+      <a class="item active" title="Phone" href="{{ url('product/tag/'.$tag->product_tags_en)}}">{{str_replace(',',' ', $tag->product_tags_en) }} </a> 
+      @endforeach
+      <!-- /.tag-list -->
+     </div>
+     <!-- /.sidebar-widget-body -->
     </div>
-    <!-- /.sidebar-widget-body -->
-   </div>
-</div>
-   <!-- /.sidebar-widget -->
-   <!-- ============================================== PRODUCT TAGS : END ============================================== -->

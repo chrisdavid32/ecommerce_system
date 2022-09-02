5cf6258bfc002d12383a0b9e32c415af50b8a63a
chrisdavid32 <chrisdavid3271@gmail.com>
2022-09-02 13:44:46 +0100

    wip

--- resources/views/frontend/payment/stripe.blade.php
+++ resources/views/frontend/payment/stripe.blade.php
@@ -92,6 +92,15 @@ Stripe Payment
                                     </div>
                                     <form action="{{route('stripe.order')}}" method="post" id="payment-form">
                                         @csrf
+                                        <input type="text" name="shipping_name" value="{{$data['shipping_name']}}">
+                                        <input type="text" name="shipping_email" value="{{$data['shipping_email']}}">
+                                        <input type="text" name="shipping_phone" value="{{$data['shipping_phone']}}">
+                                        <input type="text" name="post_code" value="{{$data['post_code']}}">
+                                        <input type="text" name="district_id" value="{{$data['district_id']}}">
+                                        <input type="text" name="district_id" value="{{$data['division_id']}}">
+                                        <input type="text" name="state_id" value="{{$data['state_id']}}">
+                                        <input type="text" name="notes" value="{{$data['notes']}}">
+
                                     <div class="form-row">
                                         <label for="card-element">
                                         Credit or debit card

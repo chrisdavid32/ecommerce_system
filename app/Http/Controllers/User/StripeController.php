5c0c2408362cd329360e79047c43b1a44b2b2150
chrisdavid32 <chrisdavid3271@gmail.com>
2022-09-05 11:38:36 +0100

    adding order and order item

--- app/Http/Controllers/User/StripeController.php
+++ app/Http/Controllers/User/StripeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\user;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Support\Facades\Session;
 use Gloudemans\Shoppingcart\Facades\Cart;
 
@@ -28,7 +29,35 @@ class StripeController extends Controller
         'currency' => 'usd',
         'description' => 'Chrisdave Store',
         'source' => $token,
-        'metadata' => ['order_id' => '6735'],
+        'metadata' => ['order_id' => uniqid()],
+        ]);
+
+        $order_id = Order::insertGetId([
+            'user_id' => auth()->user()->id,
+            'district_id' => $request->district_id,
+            'division_id' => $request->division_id,
+            'state_id' => $request->state_id,
+            'name' => $request->shipping_name,
+            'email' => $request->shipping_email,
+            'phone' => $request->shipping_phone,
+            'post_code' => $request->post_code,
+            'notes' => $request->notes,
+            'payment_type' => 'Stripe',
+            'payment_method' => $charge->payment_method,
+            'transaction_id' => $charge->balance_transaction,
+            'currency' => $charge->currency,
+            'amount' => $total_amount,
+            'order_number' => $charge->metadata->order_id,
+            'invoice_no' => $request->notes,
+            'order_date' => $request->notes,
+            'comfirmed_date' => $request->notes,
+            'processded_date' => $request->notes,
+            'pick_date' => $request->notes,
+            'shipped_date' => $request->notes,
+            'cancel_date' => $request->notes,
+            'returned_date' => $request->notes,
+            'returned_reason' => $request->notes,
+            'status' => $request->notes,
         ]);
 
         // dd($charge);

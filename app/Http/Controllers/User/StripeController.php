51c3f8ad3430ffdd9a6dba481a62f63ff6a67d93
chrisdavid32 <chrisdavid3271@gmail.com>
2022-09-05 13:48:05 +0100

    fixed stripe transaction

--- app/Http/Controllers/User/StripeController.php
+++ app/Http/Controllers/User/StripeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\user;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
+use App\Models\OrderItem;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Session;
 use Gloudemans\Shoppingcart\Facades\Cart;
 
@@ -48,18 +50,39 @@ class StripeController extends Controller
             'currency' => $charge->currency,
             'amount' => $total_amount,
             'order_number' => $charge->metadata->order_id,
-            'invoice_no' => $request->notes,
-            'order_date' => $request->notes,
-            'comfirmed_date' => $request->notes,
-            'processded_date' => $request->notes,
-            'pick_date' => $request->notes,
-            'shipped_date' => $request->notes,
-            'cancel_date' => $request->notes,
-            'returned_date' => $request->notes,
-            'returned_reason' => $request->notes,
-            'status' => $request->notes,
+            'invoice_no' => 'CDS'. mt_rand(1000000,99999999),
+            'order_date' => Carbon::now()->format('d F Y'),
+            // 'comfirmed_date' => $request->notes,
+            // 'processded_date' => $request->notes,
+            // 'pick_date' => $request->notes,
+            // 'shipped_date' => $request->notes,
+            // 'cancel_date' => $request->notes,
+            // 'returned_date' => $request->notes,
+            // 'returned_reason' => $request->notes,
+            'status' => 'Pending',
         ]);
 
-        // dd($charge);
+        $carts = Cart::content();
+        foreach ($carts as $cart) {
+           OrderItem::create([
+            'order_id' => $order_id,
+            'product_id' => $cart->id,
+            'color' => $cart->options->color,
+            'size' => $cart->options->size,
+            'qty' => $cart->qty,
+            'price' => $cart->price
+           ]);
+        }
+
+        if(Session::has('coupon')){
+            Session::forget('coupon');
+        }
+        Cart::destroy();
+
+        $notification = [
+            'message' => 'Order Place Successfully',
+            'alert-type' => 'success'
+        ];
+        return redirect()->route('dashboard')->with($notification);
     }
 }

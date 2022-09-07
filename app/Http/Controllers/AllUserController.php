dfc2e8e6743a958a90099f09efb7add7dae40f04
chrisdavid32 <chrisdavid3271@gmail.com>
2022-09-07 18:45:05 +0100

    wip

--- app/Http/Controllers/AllUserController.php
+++ app/Http/Controllers/AllUserController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class AllUserController extends Controller
 {
     public function orderList()
     {
-        
+        $orders = Order::where('user_id', auth()->user()->id)->orderBy('id', 'DESC')->get();
+        return view('frontend.user.order_list', compact('orders'));
     }
 }

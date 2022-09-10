ea9ae92aa08a5ee5332b0d3bb68ab48f5985f98d
chrisdavid32 <chrisdavid3271@gmail.com>
2022-09-11 00:15:49 +0100

    add set paper

--- app/Http/Controllers/AllUserController.php
+++ app/Http/Controllers/AllUserController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Models\Order;
 use App\Models\OrderItem;
 use Illuminate\Http\Request;
-use PDF;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class AllUserController extends Controller
 {
@@ -26,7 +27,7 @@ class AllUserController extends Controller
     {
         $order = Order::with('division', 'district', 'state', 'user')->where('id', $order_id)->where('user_id', auth()->user()->id)->first();
         $order_item = OrderItem::with('product')->where('order_id', $order_id)->orderBy('id', 'DESC')->get();
-        $pdf = Pdf::loadView('frontend.user.invoice', compact('order', 'order_item'));
+        $pdf = Pdf::loadView('frontend.user.invoice', compact('order', 'order_item'))->setPaper('a4');
         return $pdf->download('invoice.pdf');
     }
 }

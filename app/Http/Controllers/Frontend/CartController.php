6c6d060c3da54664a6195df6e692ecd81ef69aaf
chrisdavid32 <chrisdavid3271@gmail.com>
2022-09-10 18:42:05 +0100

    cash payment method

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -195,7 +195,7 @@ class CartController extends Controller
     }elseif($request->payment_method == 'card'){
         return 'card';
     }else{
-        return 'cash'; 
+        return view('frontend.payment.cash', compact('data', 'cartTotal'));
     }
    }
 }
